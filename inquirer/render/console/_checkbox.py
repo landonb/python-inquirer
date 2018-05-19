7ef8f0e6cc035055b1f89621879dbc13ffb62d36
landonb <landonb@users.noreply.github.com>
2018-05-18 19:48:13 -0500

    WIP

--- inquirer/render/console/_checkbox.py
+++ inquirer/render/console/_checkbox.py
@@ -1,16 +1,26 @@
 # -*- coding: utf-8 -*-
+# vim:tw=0:ts=4:sw=4:et
 
 from readchar import key
 from .base import BaseConsoleRender
 from inquirer import errors
 
 
+CTRL_RIGHT_SEQ = ['\x1b[1', ';', '3', 'C']
+
+CTRL_LEFT_SEQ = ['\x1b[1', ';', '5', 'C']
+
+
 class Checkbox(BaseConsoleRender):
     def __init__(self, *args, **kwargs):
+#
+        import pdb;pdb.set_trace()
+
         super(Checkbox, self).__init__(*args, **kwargs)
         self.selection = [k for (k, v) in enumerate(self.question.choices)
                           if v in (self.question.default or [])]
         self.current = 0
+        self.key_state = []
 
     def get_options(self):
         for n in range(len(self.question.choices)):
@@ -53,4 +63,38 @@ class Checkbox(BaseConsoleRender):
                 result.append(getattr(value, 'value', value))
             raise errors.EndOfInput(result)
         elif pressed == key.CTRL_C:
+            self.key_state = []
             raise KeyboardInterrupt()
+        else:
+            self.process_input_combo(pressed)
+            return
+        self.key_state = []
+
+    def process_input_combo(self, pressed):
+        if not self.key_state:
+            self.key_state.append(pressed)
+        else:
+            match_len = self.match_state(CTRL_RIGHT_SEQ)
+            if match_len == len(CTRL_RIGHT_SEQ):
+                # FIXME: Implement
+                pass
+            elif match_len > 0:
+                return
+
+            match_len = self.match_state(CTRL_LEFT_SEQ)
+            if match_len == len(CTRL_LEFT_SEQ):
+                # FIXME: Implement
+                pass
+            elif match_len > 0:
+                return
+
+    def match_state(self, known_seq):
+        match_len = 0
+        for posit in range(len(self.key_state)):
+            if self.key_state[posit] == known_seq[posit]:
+                match_len += 1
+            else:
+                match_len = 0
+                break
+        return match_len
+
